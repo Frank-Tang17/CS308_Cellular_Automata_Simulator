64f4581b198aefffc919730830708bad46889faa
Michael Dodd <mmd61@duke.edu>
2020-01-27 23:01:19 -0500

    Made Cell.java an abstract class and added comments for methods

--- src/cellsociety/Cell.java
+++ src/cellsociety/Cell.java
@@ -7,14 +7,28 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
-public class Cell {
+/**
+ * Cell acts as a node of the grid for the simulation
+ */
+public abstract class Cell {
     private int myRow, myCol;
     private int myState;
     private Rectangle myRect;
     private ImageView myImage; // This may be on a subclass by subclass basis
-    private static int[] neighborColIndex = {0, 1, 0, -1};
-    private static int[] neighborRowIndex = {-1, 0, 1, 0};
+    private static int[] neighborColIndex; // Dependent on cell type ------ = {0, 1, 0, -1};
+    private static int[] neighborRowIndex; // Dependent on cell type ------ = {-1, 0, 1, 0};
 
+    /**
+     * Constructor for master class Cell object
+     *
+     * @param row the cells row in the grid
+     * @param col the cells col in the grid
+     * @param xCoor the x-coordinate of the upper left hand corner of the cell
+     * @param yCoor the y-coordinate of the upper left hand corner of the cell
+     * @param width the width of the cell
+     * @param height the height of the cell
+     * @param startingState the starting state of the cell
+     */
     public Cell(int row, int col, int xCoor, int yCoor, int width, int height, int startingState) {
         myState = startingState;
         myRow = row;
@@ -24,26 +38,50 @@ public class Cell {
         myRect.setStroke(Color.BLACK);
 ;    }
 
+    /**
+     * Getter method for row and column
+     *
+     * @return array of Cell row and col in that order
+     */
     public int[] getRowAndCol() {
         return new int[] {myRow, myCol};
     }
 
+    /**
+     * Returns the states of each neighboring cell
+     *
+     * @param theGrid current Grid to update state based on
+     * @return array of neighboring states
+     */
     private int[] getNeighborStates(Grid theGrid) {
         int[] neighborStates = new int [neighborColIndex.length];
         for (int i = 0; i < neighborColIndex.length; i++) {
-            neighborStates[i] = theGrid.getMyGrid[myRow + neighborRowIndex[i]][myCol + neighborColIndex[i]].getCurrentState();
+            if (theGrid.validIndex(myRow + neighborRowIndex[i], myCol + neighborColIndex[i])) {
+                neighborStates[i] = theGrid.getMyGrid[myRow + neighborRowIndex[i]][myCol + neighborColIndex[i]].getCurrentState();
+            }
         }
         return neighborStates;
     }
 
-    public void update(Grid theGrid) {
+    /**
+     * Cell type dependent method that changes the current state of the cell
+     *
+     * @param theGrid current Grid to update state based on
+     */
+    public abstract void update(Grid theGrid);
 
-    }
-
+    /**
+     * Getter method for the current state of the Cell
+     *
+     * @return the current state
+     */
     public int getCurrentState() {
         return myState;
     }
 
+    /**
+     *
+     */
     private void setCellImage() {
 
     }

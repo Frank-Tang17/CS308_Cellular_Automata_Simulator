5540b051ca0c70a8a426b16c0157b5f7d893d91d
Michael Dodd <mmd61@duke.edu>
2020-01-27 22:13:34 -0500

    Implemented constructor and getNeighborStates() for Cell.java

--- src/cellsociety/Cell.java
+++ src/cellsociety/Cell.java
@@ -1,33 +1,47 @@
 package cellsociety;
 
+import javax.swing.text.html.ImageView;
 import java.awt.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 public class Cell {
     private int myRow, myCol;
-    private int state;
+    private int myState;
     private Rectangle myRect;
+    private ImageView myImage; // This may be on a subclass by subclass basis
+    private static int[] neighborColIndex = {0, 1, 0, -1};
+    private static int[] neighborRowIndex = {-1, 0, 1, 0};
 
-    public Cell() {
+    public Cell(int row, int col, int xCoor, int yCoor, int width, int height, int startingState) {
+        myState = startingState;
+        myRow = row;
+        myCol = col;
+        myRect = new Rectangle(xCoor, yCoor, width, height);
+        myRect.setFill(Color.WHITE);
+        myRect.setStroke(Color.BLACK);
+;    }
+
+    public int[] getRowAndCol() {
+        return new int[] {myRow, myCol};
+    }
+
+    private int[] getNeighborStates(Grid theGrid) {
+        int[] neighborStates = new int [neighborColIndex.length];
+        for (int i = 0; i < neighborColIndex.length; i++) {
+            neighborStates[i] = theGrid.getMyGrid[myRow + neighborRowIndex[i]][myCol + neighborColIndex[i]].getCurrentState();
+        }
+        return neighborStates;
+    }
+
+    public void update(Grid theGrid) {
 
     }
 
-    private int[] getRowAndCol() {
-
-        return null;
-    }
-
-    private int[] getNeighborStates() {
-
-        return null;
-    }
-
-    public void update() {
-
-    }
-
-    private int getCurrentState() {
-
-        return 0;
+    public int getCurrentState() {
+        return myState;
     }
 
     private void setCellImage() {

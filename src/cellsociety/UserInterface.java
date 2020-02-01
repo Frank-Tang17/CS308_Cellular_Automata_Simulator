5c8f96e59ee4b63a12c8c3620a85487020601ac7
Frank Tang <ft39@duke.edu>
2020-02-01 05:15:27 -0500

    Continued work on GUI. Currently has buttons enables onto the interface with basic CSS.

--- src/cellsociety/UserInterface.java
+++ src/cellsociety/UserInterface.java
@@ -1,15 +1,27 @@
 package cellsociety;
+import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.web.WebView;
+import javax.imageio.ImageIO;
 
 
 /**
@@ -22,7 +34,7 @@ public class UserInterface {
   private static final String RESOURCES = "resources";
   public static final String DEFAULT_RESOURCE_PACKAGE = RESOURCES + ".";
   public static final String DEFAULT_RESOURCE_FOLDER = "/" + RESOURCES + "/";
-  public static final String STYLESHEET = "default.css";
+  public static final String STYLESHEET = "userInterface.css";
   public static final String BLANK = " ";
 
 
@@ -44,11 +56,11 @@ public class UserInterface {
   private Text titleDisplay = new Text();
   private Text frameDisplay = new Text();
 
-  Button pauseButton = new Button("Pause/Resume");
-  Button forwardButton = new Button("Step Forward");
-  Button resetButton = new Button("Reset");
-  Button speedUpButton = new Button("Speed Up");
-  Button speedDownButton = new Button("Speed Down");
+  private Button pauseButton;
+  private Button forwardButton;
+  private Button resetButton;
+  private Button speedUpButton;
+  private Button slowDownButton;
 
   private ResourceBundle myResources;
 
@@ -81,6 +93,7 @@ public class UserInterface {
   // Create the game's "scene": what shapes will be in the game and their starting properties
   public Scene setupUserInterface(int width, int height) {
     BorderPane root = new BorderPane();
+    root.setBottom(makeSimulationControlPanel());
     // must be first since other panels may refer to page
 //    root.setCenter(makePageDisplay());
 //    root.setTop(makeInputPanel());
@@ -90,12 +103,72 @@ public class UserInterface {
     // create scene to hold UI
     Scene scene = new Scene(root, width, height);
     // activate CSS styling
-   // scene.getStylesheets().add(getClass().getResource(DEFAULT_RESOURCE_FOLDER + STYLESHEET).toExternalForm());
+    scene.getStylesheets().add(getClass().getResource(DEFAULT_RESOURCE_FOLDER + STYLESHEET).toExternalForm());
 
     return scene;
   }
 
+  // makes a button using either an image or a label
+  private Button makeButton (String property, EventHandler<ActionEvent> handler) {
+    // represent all supported image suffixes
+    final String IMAGEFILE_SUFFIXES = String.format(".*\\.(%s)", String.join("|", ImageIO.getReaderFileSuffixes()));
+    Button result = new Button();
+    String label = myResources.getString(property);
+    if (label.matches(IMAGEFILE_SUFFIXES)) {
+      result.setGraphic(new ImageView(new Image(getClass().getResourceAsStream(DEFAULT_RESOURCE_FOLDER + label))));
+    }
+   else {
+      result.setText(label);
+    }
+    result.setOnAction(handler);
+    return result;
+  }
+
+  // Display given message as an error in the GUI
+  private void showError (String message) {
+    Alert alert = new Alert(AlertType.ERROR);
+    alert.setTitle(myResources.getString("ErrorTitle"));
+    alert.setContentText(message);
+    alert.showAndWait();
+  }
+
+  // only enable buttons when useful to user
+  private void enableButtons () {
+   // forwardButton.setDisable(!runSimulation);
+
+  }
+
+//  private Node makeSimulationDisplayPanel () {
+//    HBox result = new HBox();
+//
+//
+//    return result;
+//  }
+
+  // make the panel where "would-be" clicked URL is displayed
+  private Node makeSimulationControlPanel () {
+    HBox controlPanel = new HBox();
+    pauseButton = makeButton("pauseButton", e -> Simulator.pauseResume());
+    controlPanel.getChildren().add(pauseButton);
+
+    forwardButton = makeButton("forwardButton", e -> Simulator.pauseResume());
+    controlPanel.getChildren().add(forwardButton);
+
+    resetButton = makeButton("resetButton", e -> Simulator.pauseResume());
+    controlPanel.getChildren().add(resetButton);
+
+    speedUpButton = makeButton("speedUpButton", e -> Simulator.pauseResume());
+    controlPanel.getChildren().add(speedUpButton);
+
+    slowDownButton = makeButton("slowDownButton", e -> Simulator.pauseResume());
+    controlPanel.getChildren().add(slowDownButton);
+
+    // BLANK must be non-empty or status label will not be displayed in GUI
+    return controlPanel;
+  }
+
   public void setUpButtons() {
+
     pauseButton.setFont(Font.font(15));
     pauseButton.setLayoutX(60);
     pauseButton.setLayoutY(gameStatusDisplayBottom.getY() + 15);
@@ -120,11 +193,11 @@ public class UserInterface {
     speedUpButton.setLayoutY(gameStatusDisplayBottom.getY() + 60);
     speedUpButton.setOnAction(e -> Simulator.speedUpSimulation());
 
-    speedDownButton.setFont(Font.font(15));
-    speedDownButton.setLayoutX(240);
-    speedDownButton.setPrefWidth(120);
-    speedDownButton.setLayoutY(gameStatusDisplayBottom.getY() + 60);
-    speedDownButton.setOnAction(e -> Simulator.slowDownSimulation());
+    slowDownButton.setFont(Font.font(15));
+    slowDownButton.setLayoutX(240);
+    slowDownButton.setPrefWidth(120);
+    slowDownButton.setLayoutY(gameStatusDisplayBottom.getY() + 60);
+    slowDownButton.setOnAction(e -> Simulator.slowDownSimulation());
 
 
   }

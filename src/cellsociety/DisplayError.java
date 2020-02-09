6f94e5d5266ce2df674d1ff966f58afa10e80bca
Frank Tang <ft39@duke.edu>
2020-02-09 07:25:41 -0500

    Realized that it is probably better to only have one properties file in English instead of multiple English property files.

--- src/cellsociety/DisplayError.java
+++ src/cellsociety/DisplayError.java
@@ -5,11 +5,10 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
 public class DisplayError {
-  private static final String ERROR_EXTENSION = "ErrorMessages";
   ResourceBundle errorEnglishResources;
 
   public DisplayError(String language, String message){
-    errorEnglishResources = ResourceBundle.getBundle(language + ERROR_EXTENSION);
+    errorEnglishResources = ResourceBundle.getBundle(language);
     Alert alert = new Alert(AlertType.ERROR);
     alert.setTitle(errorEnglishResources.getString("ErrorTitle"));
     alert.setContentText(errorEnglishResources.getString(message));
